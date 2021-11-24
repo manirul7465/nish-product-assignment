f67fffd67a3be1d74135eb3293e498585f7bbc83
manirul7465 <manir7465@gmail.com>
2021-11-24 11:53:06 +0600

    clear banner

--- src/Pages/Home/Bannar.js
+++ src/Pages/Home/Bannar.js
@@ -7,7 +7,7 @@ const Img = styled('img')({
 const Bannar = () => {
     return (
         <div>
-            <h1>banner</h1>
+            
             <Img alt="" style={{ width: '100%', height: '500px' }} src="https://i.ibb.co/BqHs2jf/maxresdefault.jpg" />
         </div>
     );
